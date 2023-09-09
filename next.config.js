1b34f7bfb8e38cc3f2ebe1b21e55ff3227faf5fc
KAMRUZZAMAN <kzamanbn@gmail.com>
2023-09-09 16:04:40 +0600

    update build directory

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: process.env.NODE_ENV === 'production' ? 'export' : 'standalone',
+    distDir: 'build',
+    poweredByHeader: false,
 };
 
 module.exports = nextConfig;
