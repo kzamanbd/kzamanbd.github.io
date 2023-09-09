98480b7d82024a0126dc6ff1c759432141e3681a
KAMRUZZAMAN <kzamanbn@gmail.com>
2023-09-09 18:16:34 +0600

    added build directory based on environment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: process.env.NODE_ENV === 'production' ? 'export' : 'standalone',
+    distDir: process.env.NODE_ENV === 'production' ? '.next' : 'build',
     poweredByHeader: false,
 };
 
