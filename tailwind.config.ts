92de944bf490a066ac1fa1b96d4583a22411b181
KAMRUZZAMAN <kzamanbn@gmail.com>
2023-10-27 20:12:53 +0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-import type { Config } from 'tailwindcss';
-import colors from 'tailwindcss/colors';
+import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 const config: Config = {
     content: [
-        './pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './components/**/*.{js,ts,jsx,tsx,mdx}',
-        './app/**/*.{js,ts,jsx,tsx,mdx}',
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}"
     ],
     theme: {
         extend: {
             colors: {
                 light: {
-                    gray: '#F5F8FA',
+                    gray: "#F5F8FA"
                 },
-                primary: colors.sky,
+                primary: colors.sky
             },
             fontSize: {
-                sm: '12px',
-            },
-        },
+                sm: "12px"
+            }
+        }
     },
-    plugins: [],
+    plugins: []
 };
 
 export default config;
