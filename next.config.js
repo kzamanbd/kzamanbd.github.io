93c8c7e7197edba7e663ea2a3ff21228624e1858
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-01-11 23:40:08 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: process.env.NODE_ENV === 'production' ? 'export' : 'standalone',
+    output: 'export',
     distDir: process.env.NODE_ENV === 'production' ? '.next' : 'build',
-    poweredByHeader: false,
+    poweredByHeader: false
 };
 
 module.exports = nextConfig;
