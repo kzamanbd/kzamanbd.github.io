521d1f8429000308537dc5f08b400f0d4fbf0c94
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-05-07 15:18:42 +0600

    added projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from './components/Portfolio/HeroSection';
+import Projects from './components/Portfolio/Projects';
 
 export default function Portfolio() {
     return (
         <div className="portfolio">
             <HeroSection />
+            <Projects />
         </div>
     );
 }
